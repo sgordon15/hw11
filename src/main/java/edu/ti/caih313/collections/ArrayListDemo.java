a814201e08e7db6fd2ee30793fcd5e08806e04b7
meahtc <55120690+meahtc@users.noreply.github.com>
2020-05-04 22:04:33 -0500

    Initial commit

---
+++ src/main/java/edu/ti/caih313/collections/ArrayListDemo.java
@@ -0,0 +1,32 @@
+package edu.ti.caih313.collections;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ArrayListDemo {
+    public static void main(String[] args) {
+        List<String> toDoList = new LinkedList<>();
+        System.out.println("Enter items for the list, when prompted.");
+        boolean done = false;
+        Scanner keyboard = new Scanner(System.in);
+        while (!done) {
+            System.out.println("Type an entry:");
+            String entry = keyboard.nextLine();
+            toDoList.add(entry);
+            System.out.print("More items for the list? ");
+            String ans = keyboard.nextLine();
+            if (!ans.equalsIgnoreCase("yes")) {
+                done = true;
+            }
+        }
+        System.out.println("The list contains:");
+        int listSize = toDoList.size();
+        for (int position = 0; position < listSize; position++) {
+            System.out.println("To Do #" + (position+1) + ": " + toDoList.get(position));
+        }
+    }
+}
+
+
---
+++ src/main/java/edu/ti/caih313/collections/HashSetDemo.java
@@ -0,0 +1,31 @@
+package edu.ti.caih313.collections;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSetDemo {
+    public static void main(String[] args) {
+        Set<Integer> intSet = new HashSet<Integer>();
+
+        boolean didItAdd = intSet.add(2); //intSet.add(new Integer(2));
+        didItAdd = intSet.add(7);
+        didItAdd = intSet.add(7);    // Ignored since 7 is already in the set
+        didItAdd = intSet.add(3);
+        printSet(intSet);
+
+        intSet.remove(3);
+        printSet(intSet);
+
+        System.out.println("Set contains 2: " +
+                intSet.contains(2));
+        System.out.println("Set contains 3: " +
+                intSet.contains(3));
+    }
+
+    public static void printSet(Set<Integer> intSet) {
+        System.out.println("The set contains:");
+        for (Integer num : intSet) {
+            System.out.println(num.intValue());
+        }
+    }
+}
